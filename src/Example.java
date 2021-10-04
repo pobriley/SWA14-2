68f485ad0710869e852e4b1053f90614ad6238f7
pobriley <624259030@webmail.npru.ac.th>
2021-10-04 16:56:15 +0700

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String[] args)
+    {
+  
+        // This is the "method chaining".
+        new A().setint(10).setfloat(20).display();
+    }
+}
